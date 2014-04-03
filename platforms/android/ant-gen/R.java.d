576d53c93534555dbb25bfbbed7a0c6c6283d643
Tomo Masakura <tomo.masakura@gmail.com>
2014-04-04 08:44:11 +0900

    無視リストを調整

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -6,4 +6,9 @@
 /Users/masakura/works/hoge1/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/masakura/works/hoge1/platforms/android/res/values/strings.xml \
 /Users/masakura/works/hoge1/platforms/android/res/xml/config.xml \
+/Users/masakura/works/hoge1/platforms/android/ant-build/res/drawable/icon.png \
+/Users/masakura/works/hoge1/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/masakura/works/hoge1/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/masakura/works/hoge1/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/masakura/works/hoge1/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/masakura/works/hoge1/platforms/android/ant-build/AndroidManifest.xml \
