491d6593ec425685fd58ae824038a30272cf916c
Tomo Masakura <tomo.masakura@gmail.com>
2014-04-04 10:14:31 +0900

    間違いコード

--- test.rb
+++ test.rb
@@ -1,11 +1,11 @@
 reinstall_apps
 start_test_server_in_background
 sleep(5)
-set_text "webView css:'#left'", "13"
-set_text "webView css:'#right'", "9"
+set_text "webView css:'#left'", "17"
+set_text "webView css:'#right'", "015"
 button = query("cordovaWebView css:'#calculate'").first
 touch(button)
 sleep(2)
-raise "assert" unless query("cordovaWebView css:'#result'").first["textContent"] == "22"
+raise "assert" unless query("cordovaWebView css:'#result'").first["textContent"] == "32"
 quit
 
--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -2,9 +2,6 @@ $(function() {
     'use strict';
 
     $('#calculate').on('click', function() {
-        console.log('hogehoge');
-        console.log($('#left').val());
-        console.log($('#right').val());
-        $('#result').text(parseInt($('#left').val(), 10) + parseInt($('#right').val(), 10));
+        $('#result').text($('#left').val() + $('#right').val());
     });
 });
