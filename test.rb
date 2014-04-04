5ea01c47c763425abc66403d4fc70db5a0a20ec7
Tomo Masakura <tomo.masakura@gmail.com>
2014-04-04 09:50:20 +0900

    足し算機能を追加

--- test.rb
+++ test.rb
@@ -1,6 +1,10 @@
 reinstall_apps
 start_test_server_in_background
 sleep(5)
-query("cordovaWebView css:'h1'").first
+set_text "webView css:'#left'", "13"
+set_text "webView css:'#right'", "9"
+button = query("cordovaWebView css:'#calculate'").first
+touch(button)
+sleep(2)
 quit
 
--- yeoman/app/scripts/main.js
+++ yeoman/app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('\'Allo \'Allo!');
+$(function() {
+    'use strict';
+
+    $('#calculate').on('click', function() {
+        console.log('hogehoge');
+        console.log($('#left').val());
+        console.log($('#right').val());
+        $('#result').text(parseInt($('#left').val(), 10) + parseInt($('#right').val(), 10));
+    });
+});
